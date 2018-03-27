630860e14e7fc6560151a873977d821ab1ed300e
pisakec <pisakec@yahoo.com>
2018-03-27 15:16:29 +0700

    Update header.php

--- header.php
+++ header.php
@@ -5,7 +5,6 @@
     <meta charset="<?php bloginfo( 'charset' ); ?>"/>
     <meta name="description" content="<?php bloginfo('description'); ?>"/>
     <meta name="viewport" content="width=device-width"/>
-    <meta charset="<?php bloginfo( 'charset' ); ?>">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="mobile-web-app-capable" content="yes">
